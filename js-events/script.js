daace59fdb82dd785f74f4ad6ca0cef6c66fe05d
Melissa <mizhalpx@gmail.com>
2026-04-17 17:28:59 +0000

    yh

---
+++ js-events/script.js
@@ -0,0 +1,20 @@
+function greet(name) {
+    //console.log(`Hello ${name}`);
+    console.log("Hello " + name);
+}
+
+greet("Melissa");
+
+// function addNumbers(num1, num2){
+//     let sum = num1 + num2;
+//     alert("Sum is equal to" + sum);
+
+// }
+
+//addNumbers(6,7);
+
+function printArticle(){
+     alert("We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost.");
+
+}
+
