0511ba306611193eadf0100ddaff85a30d437900
Melissa <mizhalpx@gmail.com>
2026-04-20 17:27:27 +0000

    te

--- js-events/script.js
+++ js-events/script.js
@@ -1,20 +1,27 @@
-function greet(name) {
-    //console.log(`Hello ${name}`);
-    console.log("Hello " + name);
-}
-
-greet("Melissa");
-
-// function addNumbers(num1, num2){
-//     let sum = num1 + num2;
-//     alert("Sum is equal to" + sum);
-
+// function greet(name) {
+//     //console.log(`Hello ${name}`);
+//     console.log("Hello " + name);
 // }
+
+// greet("Melissa");
+
+ //function addNumbers(num1, num2){
+ //    let sum = num1 + num2;
+ //    alert("Sum is equal to" + sum);
+
+ //}
 
 //addNumbers(6,7);
 
-function printArticle(){
+function printArticle1(){
      alert("We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost.");
+}
 
+function printArticle2(){
+     alert("We believe that men will be punished for their own sins, and not for Adam’s transgression.");
+}
+
+function printArticle3(){
+     alert("We believe that through the Atonement of Christ, all mankind may be saved, by obedience to the laws and ordinances of the Gospel.");
 }
 
