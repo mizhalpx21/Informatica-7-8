a606496c74754ac9d2230f1fe6369f23aa5a8983
Melissa <mizhalpx@gmail.com>
2026-04-22 20:19:00 +0000

    67

---
+++ js-variable/script.js
@@ -0,0 +1,28 @@
+function userProfile(){
+    const name ="Melissa";
+    const color ="Yellow";
+    console.log(name)
+    console.log(color)
+    let loc ="Juarez Academy";
+    let mood ="Normal";
+    console.log(loc)
+    console.log(mood)
+    mood ="Happy";
+    console.log(mood)
+}
+
+function favoriteCompany(){
+    let companyName ="Intagram";
+    console.log(companyName)
+    let foundingYear="2010";
+    console.log(foundingYear)
+    let isActive=true;
+    console.log(isActive)
+    let fundingAmount;
+    console.log(fundingAmount)
+}
+
+funtion calculations(){
+    let positive=5;
+
+}
