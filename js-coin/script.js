2bb91c522bee69c71f0bc2a0c7ffb9b212c23f42
Melissa <mizhalpx@gmail.com>
2026-04-27 20:21:21 +0000

    iraafdvs

---
+++ js-coin/script.js
@@ -0,0 +1,13 @@
+function coin(){
+    if randomNum = Math.random();
+    console.log(randomNum)
+
+    let subtitle = document.querySelector("h2");
+    if(randomNum < 0.5){
+        subtitle.innerHTML ="Heads";
+        subtitle.style.color ="purple";
+    } else{
+        subtitle.innerHTML ="Tails";
+        subtitle.style.color ="pink";
+
+}
