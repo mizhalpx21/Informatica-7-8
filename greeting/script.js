0be2677cf4495d78242915673a2073a16b6e4cc0
Melissa <mizhalpx@gmail.com>
2026-05-06 19:37:48 +0000

    barby

---
+++ greeting/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector("form").onsubmit = function(){
+        let name = document.querySelector("#name").value;
+        console.log("Hi " + name);
+
+
+        return false; //Prevents website from reloading
+    };
+})
