ed8f627872d108af7a890050142d2e5a6511b9ef
Melissa <mizhalpx@gmail.com>
2026-05-09 06:40:24 +0000

    te

--- greeting/script.js
+++ greeting/script.js
@@ -8,18 +8,18 @@ document.addEventListener("DOMContentLoaded", function(){
 
         let now = new Date();
 
+
         let hour = now.getHours();
         if (hour < 12) {
             document.querySelector("#time").innerHTML = "Good morning!";
         } else if (hour < 18){
             document.querySelector("#time").innerHTML = "Good afternoon!";
         } else{
-            document.querySelector("#time").innerHTML = "Good night!";
+            document.querySelector("#time").innerHTML = "Good evening!";
         }
 
 
         let day = now.getDay();
-        console.log("Today is " + day);
 
         if(day == 0){
             document.querySelector("#day").innerHTML = "Today is Sunday";
@@ -35,8 +35,22 @@ document.addEventListener("DOMContentLoaded", function(){
             document.querySelector("#day").innerHTML = "Today is Friday";
         } else if(day == 6){
             document.querySelector("#day").innerHTML = "Today is Saturday";
+        }
 
 
+        if (day == 1) {
+        document.querySelector("#friday").innerHTML = "Not Friday, yet!";
+        } else if (day == 2) {
+        document.querySelector("#friday").innerHTML = "Not Friday, yet!";
+        } else if (day == 3) {
+        document.querySelector("#friday").innerHTML = "Not Friday, yet!";
+        } else if (day == 4) {
+        document.querySelector("#friday").innerHTML = "Not Friday, yet!";
+        } else if (day == 5) {
+        document.querySelector("#friday").innerHTML = "Finally Fridaaaaaay! 🕺";
+        } else {
+        document.querySelector("#friday").innerHTML = "Just enjoy your weekend 🙌";
+        }
 
         return false; //Prevents website from reloading after submitting
     };
