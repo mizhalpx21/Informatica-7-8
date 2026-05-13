3d9d57fb6edba0a1bb5649440d894953e4ee9415
Melissa <mizhalpx@gmail.com>
2026-05-13 02:05:08 +0000

    t

--- intervals/script.js
+++ intervals/script.js
@@ -1,13 +1,29 @@
 let counter = 0;
+let interval;
+
+function setTimer(seconds){
+    counter = seconds;
+    document.querySelector('h1').innerHTML = counter;
+
+
+}
 
 function count() {
     counter--;
     document.querySelector('h1').innerHTML = counter;
+
+    if (counter <=0){
+        clearInterval(interval);
+        document.querySelector('h1').innerHTML = "Time's Up!";
+
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').onclick = function(){
-    setInterval(count, 1000);
+        clearInterval(interval);
+        interval = setInterval(count, 1000);
+
     };
 });
 
