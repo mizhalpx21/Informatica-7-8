f3d4615db3de5b2903f004262ded26fac06ca1c4
Melissa <mizhalpx@gmail.com>
2026-05-13 02:31:34 +0000

    ra

--- intervals/script.js
+++ intervals/script.js
@@ -1,28 +1,28 @@
-let counter = 0;
+let counter = 0; //Guarda el tiempo en segundos
 let interval;
 
-function setTimer(seconds){
-    counter = seconds;
-    document.querySelector('h1').innerHTML = counter;
+function startTimer(seconds){ //Se crea una funcion que almacena los segundos de los minutos 1, 5 y 10
+    counter = seconds;  //Guarda el jumero dentro de counter
+    document.querySelector('h1').innerHTML = counter; //Esto va a mostrar el numero en la pantalla
 
 
 }
 
-function count() {
-    counter--;
-    document.querySelector('h1').innerHTML = counter;
+function count() { //Esto determina cada cuando se realiza la funcion, esto es cada segundo
+    counter--; //-1 un segundo al timer
+    document.querySelector('h1').innerHTML = counter; //Va a mostrar el numero nuevo en la pantalla
 
-    if (counter <=0){
-        clearInterval(interval);
-        document.querySelector('h1').innerHTML = "Time's Up!";
+    if (counter <=0){ //Si el contador llega a 0
+        clearInterval(interval); //Detiene el tiempo
+        document.querySelector('h1').innerHTML = "Alarm!";
 
     }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').onclick = function(){
-        clearInterval(interval);
-        interval = setInterval(count, 1000);
+    document.querySelector('#start').onclick = function(){ //Cuando haces click al boton de start counting entonces...
+        clearInterval(interval); //Detiene el timer para reiniciarlo
+        interval = setInterval(count, 1000); //Cada 1000 milisegundos es 1 segundo
 
     };
 });
